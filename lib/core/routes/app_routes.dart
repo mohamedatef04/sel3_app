4e923e0a5a6754b47cc5a9491536ff36eabf612c
mohamed atef <smsm85579@gmail.com>
2025-08-06 22:54:02 +0300

    add assets folder & theming

---
+++ lib/core/routes/app_routes.dart
@@ -0,0 +1,9 @@
+import 'package:go_router/go_router.dart';
+
+final _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+    ),
+  ],
+);
---
+++ lib/core/theme/app_styles.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/widgets.dart';
+import 'package:sel3_app/core/theme/app_colors.dart';
+
+class AppStyles {
+  static TextStyle black24 = const TextStyle(
+    fontSize: 36,
+    color: AppColors.blackColor,
+    fontWeight: FontWeight.bold,
+    fontFamily: 'IBM Plex Sans Arabic',
+  );
+  static TextStyle black14 = const TextStyle(
+    fontSize: 14,
+    color: AppColors.blackColor,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'IBM Plex Sans Arabic',
+  );
+  static TextStyle white16 = const TextStyle(
+    fontSize: 16,
+    color: AppColors.whiteColor,
+    fontWeight: FontWeight.bold,
+    fontFamily: 'IBM Plex Sans Arabic',
+  );
+}
